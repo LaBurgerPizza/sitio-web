c7a8e91d7545ce0f28b4556ce45a2b51bad04f35
LaBurgerPizza <laburgerpizzaa@gmail.com>
2026-08-06 11:39:36 -0600

    Update app.js

--- css/js/app.js
+++ css/js/app.js
@@ -161,3 +161,20 @@ behavior:"smooth"
 });
 
 });
+window.addEventListener("load",()=>{
+
+setTimeout(()=>{
+
+const loader=document.getElementById("loader");
+
+loader.style.opacity="0";
+
+setTimeout(()=>{
+
+loader.style.display="none";
+
+},800);
+
+},2000);
+
+});
